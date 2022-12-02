34221f5d1e1d3b74d920735b9bce199e23f97a6a
creamking7115 <112462309+creamking7115@users.noreply.github.com>
2022-12-02 16:48:34 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+ int a,b,c;
+ printf ( "enter any numbers: a= ");
+ scanf ("%d", &a);
+ printf ("\nenter second number: b= ");
+ scanf ("%d" , &b);
+ printf("\n enterd number are  a= %d b= %d\n",a,b);
+ c= a+b ;
+ printf("adition of these two numbers are c=%d ",c);
+ getch();
+ }
+
