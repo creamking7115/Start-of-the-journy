a77e028c8875e9ae75293eab20e06d6eb657998c
creamking7115 <112462309+creamking7115@users.noreply.github.com>
2022-12-26 16:01:55 +0530

    Add files via upload

---
+++ FABSRI.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,x=0,j,y=1;
+clrscr();
+for(i=0;i<=5;i++)
+ {
+ if(j=y)
+   j=x+y;
+   x=y;
+   y=j;
+  printf("%d",j);
+ }
+
+getch();
+}
